98be0b348b9b5ff60714831eba9ca52690a298b3
木村　幸介 <kosukekimura0528@gmail.com>
2019-06-07 21:09:49 +0900

    env: locale setting

--- config/application.rb
+++ config/application.rb
@@ -19,17 +19,10 @@ Bundler.require(*Rails.groups)
 
 module ChoreiServer
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
     config.load_defaults 5.2
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
   end
 end
