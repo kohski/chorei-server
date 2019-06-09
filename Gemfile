7294c6ccda65035d3b1a04373d6e431ac6b873c8
木村　幸介 <kosukekimura0528@gmail.com>
2019-06-09 15:15:54 +0900

    feat: add devise_token_auth on user model , /auth url

--- Gemfile
+++ Gemfile
@@ -32,6 +32,9 @@ gem 'rack-cors'
 
 gem 'rswag'
 
+gem 'devise'
+gem 'devise_token_auth'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_607_130_100) do
+ActiveRecord::Schema.define(version: 20_190_609_054_801) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -22,5 +22,26 @@ ActiveRecord::Schema.define(version: 20_190_607_130_100) do
     t.string 'image'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+  end
+
+  create_table 'users', force: :cascade do |t|
+    t.string 'provider', default: 'email', null: false
+    t.string 'uid', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.boolean 'allow_password_change', default: false
+    t.datetime 'remember_created_at'
+    t.string 'name'
+    t.string 'image'
+    t.string 'email'
+    t.text 'description'
+    t.integer 'role', default: 0
+    t.json 'tokens'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+    t.index %w[uid provider], name: 'index_users_on_uid_and_provider', unique: true
   end
 end
