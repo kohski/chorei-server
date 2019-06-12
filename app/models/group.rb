ba471dd3eeea0a297629b1721ca499447b4a19b0
木村　幸介 <kosukekimura0528@gmail.com>
2019-06-12 20:31:19 +0900

    fix: fix specs

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
 class Group < ApplicationRecord
-  after_create :join_owner_to_member
-
   validates :name, presence: true, length: { maximum: 100 }
   validates :image, base_image: true
   has_many :members, dependent: :destroy
   has_many :member_users, through: :members, source: :user
   has_many :jobs, dependent: :destroy
 
-  # def join_owner_to_member
-  #   Member.create(user_id: owner_id, group_id: id)
-  # end
+  def join_current_user_to_member(current_user)
+    members.create(user_id: current_user.id)
+  end
 end
--- db/migrate/20190612083914_remove_owner_id_column_from_group.rb
+++ db/migrate/20190612083914_remove_owner_id_column_from_group.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RemoveOwnerIdColumnFromGroup < ActiveRecord::Migration[5.2]
   def change
     remove_column :groups, :owner_id, :integer
