38598f19679026462ddc6be2dee538eec26a2138
木村　幸介 <kosukekimura0528@gmail.com>
2019-06-12 13:18:26 +0900

    feat: job association / job validation

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,61 +12,59 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_12_040707) do
-
+ActiveRecord::Schema.define(version: 20_190_612_040_707) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "groups", force: :cascade do |t|
-    t.integer "owner_id", null: false
-    t.string "name", null: false
-    t.text "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'groups', force: :cascade do |t|
+    t.integer 'owner_id', null: false
+    t.string 'name', null: false
+    t.text 'image'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "jobs", force: :cascade do |t|
-    t.integer "group_id", null: false
-    t.string "title", null: false
-    t.text "description"
-    t.text "image"
-    t.boolean "is_public", default: false, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'jobs', force: :cascade do |t|
+    t.integer 'group_id', null: false
+    t.string 'title', null: false
+    t.text 'description'
+    t.text 'image'
+    t.boolean 'is_public', default: false, null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "members", force: :cascade do |t|
-    t.integer "group_id", null: false
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'members', force: :cascade do |t|
+    t.integer 'group_id', null: false
+    t.integer 'user_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "tags", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'tags', force: :cascade do |t|
+    t.string 'name', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "provider", default: "email", null: false
-    t.string "uid", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.boolean "allow_password_change", default: false
-    t.datetime "remember_created_at"
-    t.string "name"
-    t.text "image"
-    t.string "email"
-    t.text "description"
-    t.integer "role", default: 0
-    t.json "tokens"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'provider', default: 'email', null: false
+    t.string 'uid', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.boolean 'allow_password_change', default: false
+    t.datetime 'remember_created_at'
+    t.string 'name'
+    t.text 'image'
+    t.string 'email'
+    t.text 'description'
+    t.integer 'role', default: 0
+    t.json 'tokens'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+    t.index %w[uid provider], name: 'index_users_on_uid_and_provider', unique: true
   end
-
 end
--- spec/factories/jobs.rb
+++ spec/factories/jobs.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :job do
     group_id { 1 }
-    title { "MyString" }
-    description { "MyText" }
-    image { "MyText" }
+    title { 'MyString' }
+    description { 'MyText' }
+    image { 'MyText' }
     is_public { false }
   end
 end
