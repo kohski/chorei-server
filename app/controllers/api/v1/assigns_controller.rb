24afbdeec6695ff98883bc530b55cfaba3a7817b
木村　幸介 <kosukekimura0528@gmail.com>
2019-06-13 00:26:09 +0900

    feat: assign asctions create, destroy and index

---
+++ app/controllers/api/v1/assigns_controller.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class AssignsController < ApplicationController
+      def create
+        job = Job.find_by(id: assign_params[:job_id])
+        if job.nil?
+          response_not_found(Job.name)
+          return
+        end
+
+        member = Member.find_by(id: assign_params[:member_id])
+        if member.nil?
+          response_not_found(Member.name)
+          return
+        end
+
+        if job.group.members.pluck(:id).index(current_api_v1_user.id)
+          response_not_acceptable(User.name)
+          return
+        end
+
+        if member.group.members.pluck(:id).index(member.user_id)
+          response_not_acceptable(Member.name)
+          return
+        end
+
+        assign = job.assigns.create(member_id: member.id)
+        if assign.valid?
+          response_created(assign)
+        else
+          response_bad_request(assign)
+        end
+      end
+
+      def index
+        assign_jobs = current_api_v1_user.members.map(&:assign_jobs).flatten
+        if assign_jobs.present?
+          response_success(assign_jobs)
+        else
+          response_not_found(Assign.name)
+        end
+      end
+
+      def destroy
+        assign = Assign.find_by(id: params[:id])
+        if assign.nil?
+          response_not_found(Assign.name)
+          return
+        end
+
+        if assign.job.group.members.pluck(:user_id).index(current_api_v1_user.id).nil?
+          response_not_acceptable(Assign.name)
+          return
+        end
+
+        if assign.destroy
+          response_success(assign)
+        else
+          response_bad_request(assign)
+        end
+      end
+
+      private
+
+      def assign_params
+        params.require(:assign).permit(:job_id, :member_id)
+      end
+    end
+  end
+end
--- spec/models/assign_spec.rb
+++ spec/models/assign_spec.rb
@@ -3,13 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe Assign, type: :model do
-  let(:assign){ create(:assign) }
-  context "duplicate validator" do
-    it "is invalid when duplicate register member and job" do
+  let(:assign) { create(:assign) }
+  context 'duplicate validator' do
+    it 'is invalid when duplicate register member and job' do
       job = Job.find_by(id: assign.job_id)
       another_assign = job.assigns.create(member_id: assign.member_id)
       another_assign.valid?
-      expect(another_assign.errors.full_messages[0]).to include( I18n.t('errors.messages.taken'))
+      expect(another_assign.errors.full_messages[0]).to include(I18n.t('errors.messages.taken'))
     end
   end
 end
