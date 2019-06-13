68c9e38f301dd5772d6bb1729d19405b12aaebb3
木村　幸介 <kosukekimura0528@gmail.com>
2019-06-13 15:08:50 +0900

    fix: auto_renumbering_order mthods
    後から既存orderと重複する形でorderを指定したstepが入ってきた場合は、
    updated_atが新しい場合を当該orderの前に挿入して、後ろのorderを更新する。
    その際updated_atが更新されないようにした。

--- spec/models/step_spec.rb
+++ spec/models/step_spec.rb
@@ -4,6 +4,10 @@ require 'rails_helper'
 
 RSpec.describe Step, type: :model do
   let(:step) { build(:step) }
+  let(:step_first) { create(:step, order: 0) }
+  let(:step_second) { create(:step, order: 1) }
+  let(:step_third) { create(:step, order: 2) }
+  let(:step_fource) { create(:step, order: 1) }
   context 'memo validation' do
     it 'is invalid with name over 400 characters' do
       step.memo = 'a' * 401
@@ -32,6 +36,24 @@ RSpec.describe Step, type: :model do
       step.image = 'image_dummy_text'
       step.valid?
       expect(step.errors.full_messages).to include(I18n.t('errors.format', attribute: I18n.t('activerecord.attributes.step.image'), message: I18n.t('errors.messages.wrong_mime_type')))
+    end
+  end
+  context 'auto_nubering_blank_order method' do
+    it 'is valid with gif image' do
+      step = create(:step)
+      second_step = step.job.steps.create(order: nil)
+      expect(second_step.order).to eq(step.order + 1)
+    end
+  end
+  context 'auto_set_order method' do
+    it 'is valid with gif image' do
+      step_first
+      step_second
+      step_third
+      step_fource
+      expected_memo_list = [step_first.memo, step_fource.memo, step_second.memo, step_third.memo]
+      steps = Step.all
+      expect(steps.pluck(:memo)).to eq(expected_memo_list)
     end
   end
 end
